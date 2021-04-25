f8577538485d4a9c6941838908271aadb4768e32
E6883 <55573226+E6883@users.noreply.github.com>
2021-04-26 01:20:26 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -97,8 +97,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Lord-Userbot ERROR**\n"
-                    link = "Silahkan chat: @liualvinas"
+                    text = "**Userbot ERROR**\n"
+                    link = "Silahkan chat: @xz5pd"
                     text += "Untuk melaporkan kesalahan"
                     text += f"- tinggal teruskan pesan ini {link}.\n"
                     text += "Alvin Siap Membantu Kamu\n"
